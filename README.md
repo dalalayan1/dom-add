ca6fd8fc3c1ea588618eb41f6c3bd524744253ac
SAPIENT\adala4 <adalal4@sapient.com>
2018-03-07 12:18:28 +0530

    added files

---
+++ README.md
@@ -0,0 +1,4 @@
+# dom-append
+
+Simple function which takes an object as a parameter, uses recursion and converts the object into markup.
+Second parameter(optional) is the parent element inside which the generated markup needs to be appended.
---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+     * domELementCreator - creates dom element according to input object
+     * @param {object} elementObj - object with details of new elements to be created
+     * @param {object} parentElement - parent element inside which the generated markup needs to be appended as child(optional)
+     * 
+     * @returns {object} - created dom element(s)
+     */
+    const domELementCreator = function(elementObj, parentElement) {
+
+        const { tag, attrs, children, innerText, innerHTML } = elementObj;
+        let newEle = document.createElement(tag);
+
+        if(attrs) {
+            for(var key in attrs) {
+                newEle.setAttribute(key, attrs[key]);
+            }
+        }
+
+        if(innerText) {
+            newEle.innerText = innerText;
+        }
+
+        if(innerHTML) {
+            newEle.innerHTML = innerHTML;
+        }
+        
+        if(children && children.length) {
+            children.forEach((child) => {
+                var childElement = domELementCreator(child);
+                newEle.appendChild(childElement);
+            })
+        }
+
+        if ( parentElement ) {
+            parentElement.appendChild(newEle);
+        }
+        else {
+            return newEle;
+        }
+
+
+    }
+
+
+    module.exports = domELementCreator;
